5f16e88e0dd505660d15f6228052846052cbeabe
teamstark-pro <sabarmati562@gmail.com>
2026-02-27 08:14:56 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,10 +5,6 @@ class Config:
     MONGO_URI = os.getenv("MONGO_URI") 
     OWNER_ID = int(os.getenv("OWNER_ID", "0"))
     
-    # Official Groups where bot works
-    OFFICIAL_GROUPS = set(map(int, os.getenv("OFFICIAL_GROUPS", "").split()))
-    INVITE_LINK = os.getenv("INVITE_LINK", "https://t.me/your_group_link")
-    
     # Force Subscribe Channels (Usernames or IDs separated by space)
     # Example in .env: FORCE_SUB_CHANNELS="@mychannel1 @mychannel2"
     FORCE_SUB_CHANNELS = os.getenv("FORCE_SUB_CHANNELS", "").split()
